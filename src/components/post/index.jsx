f59201c71d8253e673bd7303a99f11eb4ca7fcc9
Quinn Dumala <quinndumala@gmail.com>
2022-06-06 22:26:01 +0800

    added post buttons and caption

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -1,13 +1,25 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useRef } from "react";
+import Actions from "./actions";
+import Footer from "./footer";
 import Header from "./header";
 import Image from "./image";
 
 function Post({ content }) {
+  const commentInput = useRef(null);
+  const handleCommentInputFocus = () => commentInput.current.focus();
+
   return (
     <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
       <Header username={content.username} />
       <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        isLikedPhoto={content.isLikedByUser}
+        handleFocus={handleCommentInputFocus}
+      />
+      <Footer caption={content.caption} username={content.username} />
     </div>
   );
 }
@@ -21,6 +33,7 @@ Post.propTypes = {
     caption: PropTypes.string.isRequired,
     docId: PropTypes.string.isRequired,
     isLikedByUser: PropTypes.bool.isRequired,
+    likes: PropTypes.array.isRequired,
     comments: PropTypes.array.isRequired,
     dateCreated: PropTypes.number.isRequired,
   }),
